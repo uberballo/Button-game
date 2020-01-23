873cc8a03c8a66864c1089aefb0187ed808475ce
uberballo <kasper.paavolainen@hotmail.com>
2020-01-23 14:39:26 +0200

    add default port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 3003;
 let MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
