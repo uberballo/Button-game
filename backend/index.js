a9fdd17f0385ce8771309cdf2dc4338be652d068
uberballo <kasper.paavolainen@hotmail.com>
2020-01-23 14:35:12 +0200

    small fixes

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,5 @@ const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
+  console.log(`url: http://localhost:${config.PORT}`)
 });
